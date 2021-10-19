ae9809b3c853ccb4b0be8a4aa6898953517ce233
Floriana Domianello <floriana.domianello@gmail.com>
2021-10-19 18:16:44 +0200

    es 3 completo

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,10 @@
-let Animale = prompt("Animale preferito:\n 1. Dog\n 2. Cat\n 3. Goldfish\n 4. Monkey\n 5. Panda");
+let Animal = prompt("Animale preferito:\n 1. Dog\n 2. Cat\n 3. Goldfish\n 4. Monkey\n 5. Panda");
 
-let Identifica = (Animale == 1) ? "Chose the Dog" 
-        : (Animale == 2) ? "Chose the Cat" 
-        : (Animale == 3) ? "Chose the Goldfish" 
-        : (Animale == 4) ? "Chose the Monkey" 
-        : (Animale == 5) ? "Chose the Panda" 
+let Scelta = (Animal == 1) ? "Chose the Dog" 
+
+        : (Animal == 2) ? "Chose the Cat" 
+        : (Animal == 3) ? "Chose the Goldfish" 
+        : (Animal == 4) ? "Chose the Monkey" 
+        : (Animal == 5) ? "Chose the Panda" 
         : "Animale not found"; 
-console.log(Identifica)
+console.log(Scelta)
